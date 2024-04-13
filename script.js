8ce5143fe4b25901dd3a0be9d8be530c9991b358
Rf2025 <165812829+Rf2025@users.noreply.github.com>
2024-04-12 21:40:46 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+let buttonSearch = document.getElementById('search-btn')
+let inputVals = document.getElementById('search-bar')
+buttonSearch.addEventListener('click',function displayE(){
+        window.alert(inputVals.value)
+})
